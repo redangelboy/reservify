9174eb9bb47ba06098a69e95292ebae678d6eb50
Cesar Rojas <caesar.red@gmail.com>
2026-04-02 03:36:41 -0500

    Allow iframe embedding

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'X-Frame-Options', value: 'ALLOWALL' },
+          { key: 'Content-Security-Policy', value: "frame-ancestors *" },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
